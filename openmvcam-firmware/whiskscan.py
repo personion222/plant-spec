cab9937871323ccc9dc3595e107d9e87755a3ab3
person onion <personion222@users.noreply.github.com>
2026-03-17 11:30:27 -0400

    refactoring + new spectral imagegit add .

--- openmvcam-firmware/whiskscan.py
+++ openmvcam-firmware/whiskscan.py
@@ -3,14 +3,40 @@ from dfr180 import dfr180
 import qwiic_as7265x
 import csv
 
+
+def get_all_measurements(as7265x):
+    as7265x.take_measurements()
+    return {
+        'A': as7265x.get_calibrated_a(),
+        'B': as7265x.get_calibrated_b(),
+        'C': as7265x.get_calibrated_c(),
+        'D': as7265x.get_calibrated_d(),
+        'E': as7265x.get_calibrated_e(),
+        'F': as7265x.get_calibrated_f(),
+        'G': as7265x.get_calibrated_g(),
+        'H': as7265x.get_calibrated_h(),
+        'R': as7265x.get_calibrated_r(),
+        'I': as7265x.get_calibrated_i(),
+        'S': as7265x.get_calibrated_s(),
+        'J': as7265x.get_calibrated_j(),
+        'T': as7265x.get_calibrated_t(),
+        'U': as7265x.get_calibrated_u(),
+        'V': as7265x.get_calibrated_v(),
+        'W': as7265x.get_calibrated_w(),
+        'K': as7265x.get_calibrated_k(),
+        'L': as7265x.get_calibrated_l(),
+    }
+
+
 s1min, s1max = 60, 120
 s2min, s2max = 60, 120
 s1 = dfr180(1, minlim=s1min, maxlim=s1max)
 s2 = dfr180(2, minlim=s2min, maxlim=s2max)
 as7265x = qwiic_as7265x.QwiicAS7265x()
 as7265x.disable_indicator()
-as7265x.set_integration_cycles(128)
-fov = 5
+as7265x.set_integration_cycles(255)
+as7265x.set_gain(3)
+fov = 3
 wavelens = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'R', 'I', 'S', 'J', 'T', 'U', 'V', 'W', 'K', 'L')
 
 time.sleep(3)
@@ -24,57 +50,25 @@ for i in range(s1min, s1max, fov * 2):
     row2 = {wl: [] for wl in wavelens}
     s1.set_angle(i)
 
-    for j in range(s2min, s2max, fov):
+    for j in range(s2min, s2max + fov, fov):
         s2.set_angle(j)
         # time.sleep_ms(10)
-        as7265x.take_measurements()
-        row1['A'].append(str(as7265x.get_calibrated_a()))
-        row1['C'].append(str(as7265x.get_calibrated_c()))
-        row1['D'].append(str(as7265x.get_calibrated_d()))
-        row1['E'].append(str(as7265x.get_calibrated_e()))
-        row1['F'].append(str(as7265x.get_calibrated_f()))
-        row1['G'].append(str(as7265x.get_calibrated_g()))
-        row1['H'].append(str(as7265x.get_calibrated_h()))
-        row1['R'].append(str(as7265x.get_calibrated_r()))
-        row1['I'].append(str(as7265x.get_calibrated_i()))
-        row1['S'].append(str(as7265x.get_calibrated_s()))
-        row1['J'].append(str(as7265x.get_calibrated_j()))
-        row1['T'].append(str(as7265x.get_calibrated_t()))
-        row1['U'].append(str(as7265x.get_calibrated_u()))
-        row1['V'].append(str(as7265x.get_calibrated_v()))
-        row1['W'].append(str(as7265x.get_calibrated_w()))
-        row1['K'].append(str(as7265x.get_calibrated_k()))
-        row1['L'].append(str(as7265x.get_calibrated_l()))
-        row1['B'].append(str(as7265x.get_calibrated_b()))
+        measurements = get_all_measurements(as7265x)
+        for wl in wavelens:
+            row1[wl].append(str(measurements[wl]))
 
     s1.set_angle(i + fov)
 
-    for j in range(s2max, s2min, -fov):
+    for j in range(s2max, s2min - fov, -fov):
         s2.set_angle(j)
         # time.sleep_ms(10)
-        as7265x.take_measurements()
-        row2['A'].append(str(as7265x.get_calibrated_a()))
-        row2['C'].append(str(as7265x.get_calibrated_c()))
-        row2['D'].append(str(as7265x.get_calibrated_d()))
-        row2['E'].append(str(as7265x.get_calibrated_e()))
-        row2['F'].append(str(as7265x.get_calibrated_f()))
-        row2['G'].append(str(as7265x.get_calibrated_g()))
-        row2['H'].append(str(as7265x.get_calibrated_h()))
-        row2['R'].append(str(as7265x.get_calibrated_r()))
-        row2['I'].append(str(as7265x.get_calibrated_i()))
-        row2['S'].append(str(as7265x.get_calibrated_s()))
-        row2['J'].append(str(as7265x.get_calibrated_j()))
-        row2['T'].append(str(as7265x.get_calibrated_t()))
-        row2['U'].append(str(as7265x.get_calibrated_u()))
-        row2['V'].append(str(as7265x.get_calibrated_v()))
-        row2['W'].append(str(as7265x.get_calibrated_w()))
-        row2['K'].append(str(as7265x.get_calibrated_k()))
-        row2['L'].append(str(as7265x.get_calibrated_l()))
-        row2['B'].append(str(as7265x.get_calibrated_b()))
+        measurements = get_all_measurements(as7265x)
+        for wl in wavelens:
+            row2[wl].append(str(measurements[wl]))
 
     for wl, writer in writers.items():
         writer.writerow(row1[wl])
-        writer.writerow(row2[wl])
+        writer.writerow(row2[wl][::-1])
 
 for f in files.values():
     f.close()
